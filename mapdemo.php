1bcd58062fc6dff17e0745272c6af9517a9eacf5
Kyousuke Akimoto <chrshan@qq.com>
2017-05-04 17:03:53 +0800

    Estab

---
+++ mapdemo.php
@@ -0,0 +1,45 @@
+<?php 
+    $dbcMap = mysql_connect("localhost","root","admin");
+    if (!$dbcMap)
+    {
+        die('Can not connect:' . mysql_error());
+    }
+    //echo "here down";
+    // // if (mysql_query("CREATE DATABASE map_db",$dbcMap))
+    // // {
+    // //     echo "Database created\n";
+    // // }
+    // // else
+    // // {
+    // //     echo "Error creating database : " . mysql_error();
+    // // }
+    // // {
+    // //     echo "Database deleted";
+    // // }
+    // // else
+    // // {
+    // //     echo "Error creating database : " . mysql_error();
+    // // }
+    $dbconnect = mysql_select_db("map_db",$dbcMap);
+    if (!$dbconnect)
+    {
+        die ("Can\'t use map_db : " . mysql_error());
+    }
+    $length = $_POST['length'];
+    $end_id = mysql_query("SELECT max(Id) From Pointsave",$dbcMap);
+    $result = mysql_fetch_array($end_id,MYSQL_ASSOC);
+    for ($i=$length;$i>0;$i--)
+    {
+        $longitude = $_POST['longitude'];
+        $latitude = $_POST['latitude'];
+        $is_ok = mysql_query("INSERT INTO Pointsave (Longitude,Latitude) VALUES($longitude,$latitude)");
+    //$is_ok = mysql_query("INSERT INTO Pointsave (Longitude,Latitude) VALUES(1991,4777)");
+        if (!$is_ok)
+        {
+            die("Can\'t add map_db : " . mysql_error());
+        }
+    }
+    
+
+    mysql_close($dbcMap);          
+ ?>
